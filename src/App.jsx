f02c5f59e6a4946ab9e93619e77c4f06822d4830
Gunjan Malviya <96690710+GunjanMalviya28@users.noreply.github.com>
2024-07-03 21:32:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,15 @@
 
-import React, { useState } from 'react';
-import './App.css';
-import LoginPage from './LoginPage';
-import Dashboard from './Dashboard';
-
-function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const handleLogin = (username, password) => {
-    
-    if (username === 'Gunjan' && password === 'Gunjan@28') {
-      setIsLoggedIn(true);
-    } else {
-      setIsLoggedIn(false);
-      alert('Invalid credentials');
-    }
-  };
-
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-  };
-
-  return (
-    <div className="App">
-      {isLoggedIn ? (
-        <Dashboard onLogout={handleLogout} />
-      ) : (
-        <LoginPage onLogin={handleLogin} />
-      )}
-    </div>
-  );
-}
-
-export default App;
-
-
-// import React, { useState, useEffect } from 'react';
+// import React, { useState } from 'react';
 // import './App.css';
 // import LoginPage from './LoginPage';
 // import Dashboard from './Dashboard';
 
 // function App() {
 //   const [isLoggedIn, setIsLoggedIn] = useState(false);
-//   const [users, setUsers] = useState([]);
-
-//   useEffect(() => {
- 
-//     fetch('/db.json')
-//       .then(response => response.json())
-//       .then(data => setUsers(data.users))
-//       .catch(error => console.error('Error fetching user data:', error));
-//   }, []);
 
 //   const handleLogin = (username, password) => {
-//     const user = users.find(user => user.username === username && user.password === password);
-
-//     if (user) {
+    
+//     if (username === 'Gunjan' && password === 'Gunjan@28') {
 //       setIsLoggedIn(true);
 //     } else {
 //       setIsLoggedIn(false);
@@ -79,6 +33,52 @@ export default App;
 // }
 
 // export default App;
+
+
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import LoginPage from './LoginPage';
+import Dashboard from './Dashboard';
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+ 
+    fetch('/db.json')
+      .then(response => response.json())
+      .then(data => setUsers(data.users))
+      .catch(error => console.error('Error fetching user data:', error));
+  }, []);
+
+  const handleLogin = (username, password) => {
+    const user = users.find(user => user.username === username && user.password === password);
+
+    if (user) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+      alert('Invalid credentials');
+    }
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <div className="App">
+      {isLoggedIn ? (
+        <Dashboard onLogout={handleLogout} />
+      ) : (
+        <LoginPage onLogin={handleLogin} />
+      )}
+    </div>
+  );
+}
+
+export default App;
   
 
 
